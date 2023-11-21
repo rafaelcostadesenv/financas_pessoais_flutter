ab09e5a958a6798ff1d74af08dc53a59e0b81ec2
Rafael <rafael.costa.desenv@gmail.com>
2023-11-21 20:50:43 -0300

    ajuste

--- lib/theme/my_colors.dart
+++ lib/theme/my_colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
 class MyColors {
-  static const Color primaryColor = Colors.blueGrey;
+  static const Color primaryColor = Color.fromARGB(255, 1, 167, 251);
   static const Color textLight = Colors.white;
 }
